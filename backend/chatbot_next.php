578402e2903f7f764cfa8735e608ba6c55bdc0c1
TheSaiz <86813512+TheSaiz@users.noreply.github.com>
2025-12-08 18:03:16 -0300

    Delete backend directory

--- backend/chatbot_next.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "connection.php";
-
-$stmt = $pdo->query("SELECT * FROM chatbot_flujo ORDER BY id ASC");
-$preguntas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($preguntas);
-?>
