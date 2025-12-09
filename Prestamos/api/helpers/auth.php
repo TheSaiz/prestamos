72d70957663f3bf85eeb9a4995ad5625ca77fe4d
TheSaiz <86813512+TheSaiz@users.noreply.github.com>
2025-12-09 00:07:16 -0300

    Delete Prestamos directory

--- Prestamos/api/helpers/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-// /api/helpers/auth.php
-// Placeholder de autenticación (expandir según necesites)
-function requireAuth() {
-    // Aquí podrías validar token en headers, sesiones, etc.
-    return true;
-}
-?>
